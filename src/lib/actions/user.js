b3f901ded35db0d26539ad54a5a917be47aab090
jaafar2000 <jaafar200021@gmail.com>
2025-07-30 12:14:22 +0400

    adsf

---
+++ src/lib/actions/user.js
@@ -0,0 +1,43 @@
+import User from "../models/user.model";
+import dbConnect from "../connectdb";
+
+export const createOrUpdateUser = async (
+  id,
+  first_name,
+  last_name,
+  image_url,
+  email_addresses,
+  username
+) => {
+  try {
+    await dbConnect();
+
+    const user = await User.findOneAndUpdate(
+      { clerkId: id },
+      {
+        $set: {
+          firstName: first_name,
+          lastName: last_name,
+          avatar: image_url,
+          email: email_addresses[0].email,
+          username: username,
+        },
+      },
+      { new: true, upsert: true }
+    );
+
+    return user;
+  } catch (error) {
+    console.log('Error creating or updating user:', error);
+  }
+};
+
+export const deleteUser = async (id) => {
+  try {
+    await dbConnect();
+
+    await User.findOneAndDelete({ clerkId: id });
+  } catch (error) {
+    console.log('Error deleting user:', error);
+  }
+};
